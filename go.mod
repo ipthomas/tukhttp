c7464a7dc07152e9844be1ca110315895d996481
Ian <ian.thomas@tiani-spirit.com>
2024-03-14 18:31:43 +0000

    modified:   go.mod 	modified:   go.sum 	modified:   tukhttp.go 	modified:   vendor/github.com/ipthomas/tukutil/tukutil.go 	new file:   vendor/github.com/jrivets/gorivets/.gitignore 	new file:   vendor/github.com/jrivets/gorivets/.travis.yml 	new file:   vendor/github.com/jrivets/gorivets/LICENSE 	new file:   vendor/github.com/jrivets/gorivets/README.md 	new file:   vendor/github.com/jrivets/gorivets/global_map.go 	new file:   vendor/github.com/jrivets/gorivets/log.go 	new file:   vendor/github.com/jrivets/gorivets/lru.go 	new file:   vendor/github.com/jrivets/gorivets/sorted_slice.go 	new file:   vendor/github.com/jrivets/gorivets/utils.go 	new file:   vendor/github.com/jrivets/log4g/.gitignore 	new file:   vendor/github.com/jrivets/log4g/.travis.yml 	new file:   vendor/github.com/jrivets/log4g/LICENSE 	new file:   vendor/github.com/jrivets/log4g/README.md 	new file:   vendor/github.com/jrivets/log4g/console_appender.go 	new file:   vendor/github.com/jrivets/log4g/file_appender.go 	new file:   vendor/github.com/jrivets/log4g/layout_utils.go 	new file:   vendor/github.com/jrivets/log4g/log4g.go 	new file:   vendor/github.com/jrivets/log4g/log4g.properties 	new file:   vendor/github.com/jrivets/log4g/log_config.go 	new file:   vendor/github.com/jrivets/log4g/log_context.go 	new file:   vendor/github.com/jrivets/log4g/log_level_setting.go 	new file:   vendor/github.com/jrivets/log4g/log_manager.go 	new file:   vendor/github.com/jrivets/log4g/log_utils.go 	new file:   vendor/github.com/jrivets/log4g/logger.go 	modified:   vendor/modules.txt

--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.19
 require (
 	github.com/ipthomas/tukcnst v1.3.12
 	github.com/ipthomas/tukutil v1.3.14
+	github.com/jrivets/log4g v0.0.0-20191016233753-c02c5046dc98
 )
 
-require github.com/google/uuid v1.3.0 // indirect
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/jrivets/gorivets v0.0.0-20191016233429-669d3f1acfa1 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- tukhttp.go
+++ tukhttp.go
@@ -3,19 +3,22 @@ package tukhttp
 import (
 	"context"
 	"io"
-	"log"
 	"net/http"
 	"strings"
 	"time"
 
 	"github.com/ipthomas/tukcnst"
 	"github.com/ipthomas/tukutil"
+	"github.com/jrivets/log4g"
 )
+
+var stl = log4g.GetLogger("2T-UIS")
 
 type HTTPRequest struct {
 	Act          string
 	Version      int
 	Server       string
+	Header       http.Header
 	Method       string
 	URL          string
 	X_Api_Key    string
@@ -39,34 +42,33 @@ func NewRequest(i TukHTTPInterface) error {
 }
 func (i *HTTPRequest) newRequest() error {
 	var err error
-	var header http.Header
-	header.Add(tukcnst.ACCEPT, tukcnst.ALL)
-	header.Add(tukcnst.CONTENT_TYPE, i.ContentType)
-	header.Add(tukcnst.CONNECTION, tukcnst.KEEP_ALIVE)
-	if i.X_Api_Key != "" && i.X_Api_Secret != "" {
-		header.Add("X-API-KEY", i.X_Api_Key)
-		header.Add("X-API-SECRET", i.X_Api_Secret)
-	}
-	if i.SOAPAction != "" {
-		header.Set(tukcnst.SOAP_ACTION, i.SOAPAction)
-	}
 	if i.Timeout == 0 {
 		i.Timeout = 15
 	}
-	i.logReq(header, i.URL, string(i.Body))
-	if err = i.sendHttpRequest(header); err != nil {
-		log.Println(err.Error())
+	if err = i.sendHttpRequest(); err != nil {
+		stl.Error(err.Error())
 	}
 	i.logRsp(i.StatusCode, string(i.Response))
 	return err
 
 }
-func (i *HTTPRequest) sendHttpRequest(header http.Header) error {
+func (i *HTTPRequest) sendHttpRequest() error {
 	var err error
-	var req *http.Request
+	var req *http.Request = new(http.Request)
 	var rsp *http.Response
 	var bytes []byte
-	req.Header = header
+	req.Header.Add(tukcnst.ACCEPT, tukcnst.ALL)
+	req.Header.Add(tukcnst.CONTENT_TYPE, i.ContentType)
+	req.Header.Add(tukcnst.CONNECTION, tukcnst.KEEP_ALIVE)
+	if i.X_Api_Key != "" && i.X_Api_Secret != "" {
+		req.Header.Add("X-API-KEY", i.X_Api_Key)
+		req.Header.Add("X-API-SECRET", i.X_Api_Secret)
+	}
+	if i.SOAPAction != "" {
+		req.Header.Set(tukcnst.SOAP_ACTION, i.SOAPAction)
+	}
+	i.logReq(req.Header, i.URL, string(i.Body))
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(i.Timeout)*time.Second)
 	defer cancel()
 	if i.Method == http.MethodPost && string(i.Body) != "" {
@@ -87,16 +89,16 @@ func (i *HTTPRequest) sendHttpRequest(header http.Header) error {
 }
 func (i *HTTPRequest) logReq(headers interface{}, url string, body string) {
 	if i.DebugMode {
-		log.Printf("HTTP Request Headers")
+		stl.Debug("HTTP Request Headers")
 		tukutil.Log(headers)
-		log.Printf("\nHTTP Request\n-- URL = %s", url)
+		stl.Debug("\nHTTP Request\n-- URL = %s", url)
 		if body != "" {
-			log.Printf("\n-- Body:\n%s", body)
+			stl.Debug("\n-- Body:\n%s", body)
 		}
 	}
 }
 func (i *HTTPRequest) logRsp(statusCode int, response string) {
 	if i.DebugMode {
-		log.Printf("HTML Response - Status Code = %v\n-- Response--\n%s", statusCode, response)
+		stl.Debug("HTML Response - Status Code = %v\n-- Response--\n%s", statusCode, response)
 	}
 }
